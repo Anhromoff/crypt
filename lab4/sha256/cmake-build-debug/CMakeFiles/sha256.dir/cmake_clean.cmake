2eebe927d353ebac222d2018fcd3455c63e21e93
ponagaibo <ponagaibo@mail.ru>
2018-05-19 20:59:32 +0300

    start lab4

---
+++ lab4/sha256/cmake-build-debug/CMakeFiles/sha256.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sha256.dir/sha256.c.obj"
+  "CMakeFiles/sha256.dir/sha256_test.c.obj"
+  "sha256.pdb"
+  "sha256.exe"
+  "sha256.exe.manifest"
+  "libsha256.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sha256.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
